433133d377eaad20487c2dde13b16f4ad72ebee4
Chenx221 <chenx221@yandex.com>
2025-04-26 21:20:59 +0800

    update

--- minitool5/CustomContentInspector.cs
+++ minitool5/CustomContentInspector.cs
@@ -52,6 +52,20 @@ namespace minitool5
                         PrefixSegment.Create(0, "50 53 42 00") // PSB\0
                     }.ToSignature(),
                 },
+                                new()
+                {
+                    File = new()
+                    {
+                        Categories = [Category.Other],
+                        Description = "Live2D",
+                        Extensions = ["L2D"],
+                        MimeType = "application/x-l2d",
+                    },
+                    Signature = new Segment[]
+                    {
+                        PrefixSegment.Create(0, "50 4B 03 04 14 00 08 08") // PK...
+                    }.ToSignature(),
+                },
                 //new() // 不可靠
                 //{
                 //    File = new()
